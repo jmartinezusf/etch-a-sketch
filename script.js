02fa18d7f04da357d87b31b5ec36457da8ca8f07
Jordan Martinez <jmartinez10@usf.edu>
2022-03-13 20:22:59 -0400

    Created a button that clears the grid and a button to create a new grid + added some styling

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const container = document.querySelector('#container');
+const clearBtn = document.querySelector('.clear');
+const newBtn = document.querySelector('.new');
+let cell = document.createElement('div');
+
+createGrid(16,16);
+
+newBtn.addEventListener('click', function() {
+    let userInput = prompt('How many squares? [0 - 100]', '');
+
+    if (userInput <= 100) {
+        createGrid(userInput, userInput);
+    } 
+});
+
+function clearGrid() {
+    container.removeChild(cell);
+}
 
 
-function createGrid (row, column) {
+function createGrid(row, column) {
 
     for(i = 0; i < (row * column); i++) {
         let cell = document.createElement('div');
         container.appendChild(cell).className = 'cell';
-    }
-}
 
-createGrid(16,16);
+        clearBtn.addEventListener('click', clearGrid);
+
+        function clearGrid() {
+            container.removeChild(cell);
+            } 
+        }
+    }
+
+
+    
+
